6676f4eae82bcf2489768a844a7b87fac337fa2f
Sergio Pantoja <sergio@sergiopantoja.com>
2017-06-04 11:59:14 -0400

    Set better root paths for logged in/out users
    The main benefit of this is, if root is a restricted page, users
    won't see the "You must sign in or sign up..." flash message.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "pages#home"
+
+  devise_scope :user do
+    authenticated :user do
+      root "pages#home", as: :authenticated_root
+    end
+
+    unauthenticated do
+      root "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
+
+  # need to keep this so we can still use the `root_path` helper
+  root "pages#home"
 
 end
